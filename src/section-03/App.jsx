2bd4f5b5bf12cb23160ee385c39cd653a358cbbf
hatdog <vallentenico21@gmail.com>
2026-07-09 21:23:48 +0800

    created section 3 and header for section 3 project

---
+++ src/section-03/App.jsx
@@ -0,0 +1,11 @@
+import Lesson2 from './Lessons/Lesson2.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Lesson2 />
+    </>
+  )
+}
+
+export default App;
---
+++ src/section-03/Lessons/Lesson2.jsx
@@ -0,0 +1,9 @@
+import Header from '../lesson_components/Header.jsx'
+
+const Lesson2 = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Lesson2;
