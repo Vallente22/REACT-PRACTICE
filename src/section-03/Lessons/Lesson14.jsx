6c8ecab357ba7865d5b101ab49d736574d990a17
hatdog <vallentenico21@gmail.com>
2026-07-11 23:03:37 +0800

    state and ternary challenge added

--- src/section-03/Lessons/Lesson14.jsx
+++ src/section-03/Lessons/Lesson14.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react"
+
 const Lesson14 = () => {
-  /**
-   * Challenge: Replace the if/else below with a ternary
-   * to determine the text that should display on the page
-   */
-  const isGoingOut = true
-  
+//challenge initialize state for isgoingout as boolean
+
+  const [isGoingOut, setIsGoingOut] = useState(true)
+
+  const handleBoolean = () => {
+    setIsGoingOut(prev => !prev)
+  }
+
+
   return (
     <main>
       <h1 className="title">Do I feel like going out tonight?</h1>
-      <button className="value">{isGoingOut ? "yes" : "no"}</button>
+      <button 
+        className="value" 
+        onClick={handleBoolean}
+        aria-label={`Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it.`}
+      >{isGoingOut ? "Yes" : "No"}</button>
     </main>
   )
 }
