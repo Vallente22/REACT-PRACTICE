5b3f024153eddd5b83415c344c4592b1dec97a37
hatdog <vallentenico21@gmail.com>
2026-07-12 13:34:10 +0800

    more rep

--- src/section-03/Lessons/Lesson14prac.jsx
+++ src/section-03/Lessons/Lesson14prac.jsx
@@ -6,12 +6,25 @@ const Lesson14prac = () => {
   const counter = () => {
     setCount(prev => prev + 1)
   }
+
+  const [isGoingOut, setIsGoingOut] = useState(true);
+
+  const goingOut = () => {
+    setIsGoingOut(prev => !prev)
+  }
+
   
   return (
     <>
       <main>
         <h1>Counter: {count}</h1>
         <button onClick={counter}>Increase</button>
+        <hr />
+        <h1>Do I feel like going out tonight?</h1>
+        <button 
+          onClick={() => setIsGoingOut(prev => !prev)}
+          aria-label={`Current answer is ${isGoingOut ? "yes" : "no"}`}
+        >{isGoingOut ? "yes" : "no"}</button>
       </main>
     </>
   )
