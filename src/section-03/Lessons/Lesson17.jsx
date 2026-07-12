238fb88557824ebb6e4904dbeb923433e5340a09
hatdog <vallentenico21@gmail.com>
2026-07-12 17:33:02 +0800

    updating array in state

--- src/section-03/Lessons/Lesson17.jsx
+++ src/section-03/Lessons/Lesson17.jsx
@@ -5,11 +5,16 @@ const Lesson17 = () => {
 
   const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
   "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
-  
+
   const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
 
   function addFavoriteThing() {
-    // We'll work on this next, nothing to do here yet.
+    setMyFavoriteThings(
+      prevFavThings => [
+        ...prevFavThings, 
+        allFavoriteThings[prevFavThings.length]
+      ]
+    )
   }
   
   return (
