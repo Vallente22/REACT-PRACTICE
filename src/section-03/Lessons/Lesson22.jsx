1d3a480b35622020a9ad9861b24e77712b0b5514
hatdog <vallentenico21@gmail.com>
2026-07-14 17:54:19 +0800

    form submission

--- src/section-03/Lessons/Lesson22.jsx
+++ src/section-03/Lessons/Lesson22.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import "../lesson_styles/lesson22.css"
 
-//input can be some type of button by putting 
-//submit on type but its better practice to put a button inside the form and put submit on type
-//<button type="submit">
-// <input type="submit" value="click" name="" id="" />
+/**
+ * by default, the form is set to GET method if you dont specify what is it for
+ * and will display whatever you put in the form on the url as it tries to look for whatever u 
+ * put inside on some other site
+ * by using POST method, your input wont be displayed in url
+ * as it puts the info in a post request 
+ * 
+ */
 const Lesson22 = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData("formEl")
+    const email = formData.get("email")
+    //submit info to backend
+    formEl.reset()
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form action="">
+      <form onSubmit={handleSubmit} method="POST">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" />
         <br />
@@ -20,7 +34,6 @@ const Lesson22 = () => {
         <button type="submit">Submit</button>
       </form>
     </section>
-    
   )
 }
 
