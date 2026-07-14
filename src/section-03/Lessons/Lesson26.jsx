2d000b233f8c79a8683e838c6e4915d7ddd74da1
hatdog <vallentenico21@gmail.com>
2026-07-14 23:52:39 +0800

    get radio button value challenge

--- src/section-03/Lessons/Lesson26.jsx
+++ src/section-03/Lessons/Lesson26.jsx
@@ -2,16 +2,13 @@ import "../lesson_styles/lesson22.css"
 
 const Lesson22 = () => {
 /** 
- * other form inputs/elements:
-  defaultValue - Used to prepopulate an input with an initial value. Commonly used when editing a profile so the user's existing information is already filled in.
-  <textarea> - Used when the user needs to enter multiple lines of text, such as comments, feedback, or descriptions.
-  Radio buttons - Used when the user must choose exactly one option from a group of choices.
-  <fieldset> - Groups related form inputs together (for example, a group of radio buttons). Browsers usually display it with a border, but its main purpose is to semantically group related controls.
-  <legend> - Provides a title or label for a <fieldset>, helping users understand what the grouped inputs are for.
+ * challenge: get the value of radio button
  */
   const signUp = (formData) => {
     const email = formData.get("email")
     const password = formData.get("password")
+    const employmentStatus = formData.get("employmentStatus")
+    console.log(employmentStatus)
 
   }
 
@@ -30,22 +27,21 @@ const Lesson22 = () => {
 
         <fieldset>
           <legend>Employment Status:</legend>
-          <label htmlFor="">
-            <input type="radio" name="employmentStatus" />
+          <label htmlFor="unemployed">
+            <input id="unemployed" type="radio" name="employmentStatus" value="unemployed" />
             Unemployed
           </label>
-          <label htmlFor="">
-            <input type="radio" name="employmentStatus" />
+          <label htmlFor="part-time">
+            <input id="part-time" type="radio" name="employmentStatus" value="part-time" />
             Part-time
           </label>
-          <label htmlFor="">
-            <input type="radio" name="employmentStatus" />
+          <label htmlFor="full-time">
+            <input id="full-time" type="radio" name="employmentStatus" value="full-time" />
             Full-time
         </label>
         </fieldset>
 
         <button type="submit">Submit</button>
-
 
       </form>
     </section>
