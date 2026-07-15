ee0e9b386176dd20b34a817d70733dd289f8a1f3
hatdog <vallentenico21@gmail.com>
2026-07-15 23:50:50 +0800

    challenge render <p> conditionally

--- src/section-03/lesson_components/Joke.jsx
+++ src/section-03/lesson_components/Joke.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
 /**
- * challenge: create state isShown with boolean value of false
- * and add a button that toggles the value back and forth //done
+ * challenge only show punchline paragraph if isshown is true//done
  */
 const Joke = (props) => {
   const [isShown, setIsShown] = useState(false)
@@ -14,7 +13,7 @@ const Joke = (props) => {
   return (
     <article>
       {props.setup && <h2>Setup: {props.setup}</h2>} 
-       <p>Punchline: {props.punchline}</p>
+      {isShown && <p>Punchline: {props.punchline}</p>}
       <button onClick={showJoke}>{isShown ? "hide" : "show"}</button>
       <hr/>
     </article>
@@ -26,6 +25,11 @@ const Joke = (props) => {
 // "If the thing on the left is truthy, evaluate and return the thing on the right."
 
 <p className="punchline">Punchline: {props.punchline}</p>
+
+{isShown === true ? <p>Punchline: {props.punchline}</p> : <p></p>} < this technically works theres just better alternative which is the one used above
+isShown starts as false and toggles between true and false every time the button is clicked. 
+Since && only renders the element on the right when the expression on the left is true, it's a 
+cleaner alternative to a ternary when we only want to conditionally render an element.
 */
 
 export default Joke;
