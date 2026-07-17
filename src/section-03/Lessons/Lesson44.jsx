efbf9aa37af6a1e96f5a8a9c167fde80ec3589c0
hatdog <vallentenico21@gmail.com>
2026-07-17 21:14:59 +0800

    initialize state array

--- src/section-03/Lessons/Lesson44.jsx
+++ src/section-03/Lessons/Lesson44.jsx
@@ -1,20 +1,34 @@
+import { useState } from "react"
 import pads from "../lesson_components/pads"
+import '../lesson_styles/padschallenge.css'
 
-export default function App() {
+const Lesson44 = () => {
   /**
-   * Challenge part 1:
+   * Challenge part 1: DONE
    * 1. Initialize state with the default value of the
    *    array pulled in from pads.js
    * 2. Map over that state array and display each one
    *    as a <button> (CSS is already written for you)
    *    (Don't worry about using the "on" or "color" 
-   *    properties yet)
+   *    properties yet) 
    */
+
+  const [padsState, setPadsState] = useState(pads)
+
+  const buttonElements = padsState.map((padsItem) => {
+    return (
+      <button key={padsItem.id}></button>
+    )
+  })
+
+
   return (
     <main>
       <div className="pad-container">
-        {/* <button>s go here */}
+        {buttonElements}
       </div>
     </main>
   )
 }
+
+export default Lesson44;
