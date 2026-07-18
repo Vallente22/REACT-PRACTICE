8a042f6ceeed9b81039545033915d6dda02ca05a
hatdog <vallentenico21@gmail.com>
2026-07-18 12:30:45 +0800

    added opacity for next challenge

--- src/section-03/lesson_components/Pad.jsx
+++ src/section-03/lesson_components/Pad.jsx
@@ -1,4 +1,10 @@
 const Pad = (props) => {
+  /**
+   * Challenge part 3:
+   * Our buttons got turned off by default! Update the code
+   * so if the button is "on", it has the className of "on".
+   */
+    
   return (
     <button style={{backgroundColor: props.color}}></button>
   )
