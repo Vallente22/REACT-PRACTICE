329560b7fdd11193ed342095a8d0275cd7fe5e4c
hatdog <vallentenico21@gmail.com>
2026-07-19 13:21:04 +0800

    add toggle function challenge

--- src/section-03/Lessons/Lesson44.jsx
+++ src/section-03/Lessons/Lesson44.jsx
@@ -4,23 +4,24 @@ import '../lesson_styles/padschallenge.css'
 import Pad from "../lesson_components/Pad"
 
 const Lesson44 = (props) => {
-  /**
-   * Challenge part 2:
-   * 1. Create a separate component called "Pad" and
-   *    replace the `button` above with our <Pad /> component
-   * 2. Pass the Pad component a prop called `color` with the
-   *    value of the same name from the `padsData` objects
-   * 3. In the Pad component, apply an inline style to the <button>
-   *    to set the backgroundColor of the button.
-   * 
-   * (We'll deal with the "on" property soon)
-   */
 
   const [padsState, setPadsState] = useState(pads)
 
+  /**
+   * Challenge: Create a toggle() function that logs
+   * "clicked!" to the console
+   * 
+   * Pass that function down to each of the Pad components
+   * and set it up so when they get clicked, the function runs. DONE
+   */
+
+  const togglePad = () => {
+    console.log("clicked")
+  }
+
   const buttonElements = padsState.map((padsItem, index) => {
     return (
-      <Pad key={padsItem.id} color={padsItem.color} on={padsItem.on} />
+      <Pad key={padsItem.id} color={padsItem.color} on={padsItem.on} toggle={togglePad} />
     )
   })
 
--- src/section-03/lesson_components/Pad.jsx
+++ src/section-03/lesson_components/Pad.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
-
 const Pad = (props) => {
-  /**
-   * Challenge: Create a toggle() function that logs
-   * "clicked!" to the console
-   * 
-   * Pass that function down to each of the Pad components
-   * and set it up so when they get clicked, the function runs
-   */
-
-  const [on, setOn] = useState(props.on)
-
-  const togglePad = () => {
-    setOn(prev => !prev)
-  }
     
   return (
-    <button className={on ? "on" : undefined}  
+    <button className={props.on ? "on" : undefined}  
       style={{backgroundColor: props.color}}
-      onClick={togglePad}  
+      onClick={props.toggle}  
     ></button>
   )
 }
