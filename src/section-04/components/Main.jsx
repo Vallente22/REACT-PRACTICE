0f6e865359223f99c51bb65e2db5db40dc3b3f94
hatdog <vallentenico21@gmail.com>
2026-07-22 14:31:57 +0800

    move top, bottom text & url to state

--- src/section-04/components/Main.jsx
+++ src/section-04/components/Main.jsx
@@ -1,35 +1,37 @@
+import { useState } from "react"
+
 export default function Main() {
-  /**
-   * Challenge: move the hardcoded meme info into React
-   * state. Use an object with `topText`, `bottomText`,
-   * and `imageUrl` properties, and set the initial values to
-   * the ones hardcoded below.
-   */
+
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imgUrl: "http://i.imgflip.com/1bij.jpg"
+  })
   
   return (
     <main>
       <div className="form">
         <label>Top Text
-            <input
-              type="text"
-              placeholder="One does not simply"
-              name="topText"
-            />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+          />
         </label>
 
         <label>Bottom Text
-            <input
-              type="text"
-              placeholder="Walk into Mordor"
-              name="bottomText"
-            />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imgUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
    </main>
   )
