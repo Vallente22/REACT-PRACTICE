8834d2609e8e5c427a8d5711915588fa071fd0ca
hatdog <vallentenico21@gmail.com>
2026-07-22 14:22:11 +0800

    import header and main components

---
+++ src/section-04/projects/MemeGenerator.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Main from "../components/Main"
+
+//changes are in Main component
+
+export default function MemeGenerator() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
