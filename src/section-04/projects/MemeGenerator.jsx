a044e7e5c12277c9328ea5ef50aa7dbe8c422b7f
hatdog <vallentenico21@gmail.com>
2026-07-27 00:25:43 +0800

    added folder for easier navigation

--- src/section-04/projects/MemeGenerator.jsx
+++ src/section-04/projects/MemeGenerator.jsx
@@ -1,5 +1,5 @@
-import Header from "../components/Header"
-import Main from "../components/Main"
+import Header from "../components/MemeGeneratorComponents/Header"
+import Main from "../components/MemeGeneratorComponents/Main"
 
 //changes are in Main component
 
