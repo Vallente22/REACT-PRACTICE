8deb786a8dee3d9fc29bada030de6097c289124f
hatdog <vallentenico21@gmail.com>
2026-07-26 23:30:17 +0800

    more challenge

--- src/section-04/components/WindowTrackerComponent.jsx
+++ src/section-04/components/WindowTrackerComponent.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react"
+
 export default function WindowTrackerComponent() {
+  /**
+   * challenge: create state windowWidth and default to window.innerWidth
+   * when the window width changes, update the state
+   */
+
+  useEffect(() => {
+    window.addEventListener("resize", function() {
+      console.log("Resized!")
+    })
+  }, [])
   return(
     <h1>Window width: {window.innerWidth}</h1>
   )
