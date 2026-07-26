849c1a39fae254662bf6e5a9dbbf8ebf5fff3280
hatdog <vallentenico21@gmail.com>
2026-07-27 00:08:32 +0800

    complete challenge but theres a bug

--- src/section-04/components/WindowTrackerComponent.jsx
+++ src/section-04/components/WindowTrackerComponent.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function WindowTrackerComponent() {
   /**
@@ -6,12 +6,27 @@ export default function WindowTrackerComponent() {
    * when the window width changes, update the state
    */
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  /**
+   * There's a bug in this code:
+   * The resize event belongs to the browser, not React.
+   * React can remove the component from the screen,
+   * but the browser doesn't know that. It keeps listening
+   * for resize events unless we tell it to stop.
+   * If we don't remove the event listener, every time
+   * this component appears again, another listener is added.
+   * Eventually, one resize can trigger the same code
+   * multiple times.
+   */
+  
   useEffect(() => {
-    window.addEventListener("resize", function() {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth)
       console.log("Resized!")
     })
   }, [])
   return(
-    <h1>Window width: {window.innerWidth}</h1>
+    <h1>Window width: {windowWidth}</h1>
   )
 }
--- src/section-04/projects/WindowTracker.jsx
+++ src/section-04/projects/WindowTracker.jsx
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 import WindowTrackerComponent from "../components/WindowTrackerComponent"
 
 export default function WindowTracker() {
+  /**
+   * Challenge:
+   * 1. Create state called `show`, default to `true`
+   * 2. When the button is clicked, toggle `show`
+   * 3. Only display `<WindowTracker>` if `show` is `true`
+   */
 
   const [show, setShow] = useState(true)
 
